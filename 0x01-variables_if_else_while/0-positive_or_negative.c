7ace065de634670fb856b373d5417775a0560edf
waffle <asuka9767@protonmail.com>
2019-09-19 22:39:12 +0000

    fixed spacing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,32 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - main
+(* a blank line
+ * Program generates randon number and checks type)?
+(* stdlib.h, time.h, stdio.h)*
+ * Return: 0 to exit file
+ */
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-	if (n > 0){
-	printf("%i is positive\n", n);
-	} else if (n == 0){
-	printf("%i is zero\n", n);
-	} else {
-	printf("%i is negative\n", n);
-	}
+if (n > 0)
+{
+printf("%i is positive\n", n);
+}
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
+else
+{
+printf("%i is zero\n", n);
+}
 
-	return (0);
+return (0);
 }
