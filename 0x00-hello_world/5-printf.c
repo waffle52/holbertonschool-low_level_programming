4bdcdd3955b1f2dade4a2ae369ac1e53812b43c3
waffle <asuka9767@protonmail.com>
2019-09-19 04:06:48 +0000

    redone in proper betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,14 @@
 #include "stdio.h"
 
-/* main method */
+/**
+ * main - method that is main
+ * Description: main function that prints given string
+ * Return: returns 0
+ */
+
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
 }
