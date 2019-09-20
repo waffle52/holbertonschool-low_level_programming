ebd5105447a79a2a0c9b7ccd421fe9e4db2c64d8
waffle <asuka9767@protonmail.com>
2019-09-20 05:00:26 +0000

    fixed minor issues

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,13 @@
 
 int main(void)
 {
-	int num = 0;
-	while (num < 10)
-	{
-	putchar((num% 10) + '0');
-	num++;
-	}
+int num = 0;
+
+while (num < 10)
+{
+putchar((num % 10) + '0');
+num++;
+}
 putchar('\n');
 
 return (0);
