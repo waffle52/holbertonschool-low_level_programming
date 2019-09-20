a616b54bd14e6a95f78d8898cdaf8da9f5af597a
waffle <asuka9767@protonmail.com>
2019-09-20 04:03:59 +0000

    uploading file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: loops trhough ascii code and displays a - z, A - Z)?
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int num = 48;
+
+while (num < 58)
+{
+putchar(int num);
+num++;
+}
+
+putchar('\n');
+
+return (0);
+}
