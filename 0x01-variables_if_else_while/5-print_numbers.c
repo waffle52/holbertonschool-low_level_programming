43aeb99480b041a1d0dfaada9a203fdc3cbdfc90
waffle <asuka9767@protonmail.com>
2019-09-20 02:49:34 +0000

    uploading file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+/**
+ * main - Entry point
+ * Description: loops through numbers 1 to 9)?
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+
+while (i < 10)
+{
+printf("%i", i);
+i++;
+}
+printf("\n");
+
+return (0);
+}
