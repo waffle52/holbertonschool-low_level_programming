1fe38f1c1509d2d676822abeb9f71d3672b71278
waffle <asuka9767@protonmail.com>
2019-09-20 06:20:18 +0000

    uploading file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: loops trhough ascii code and displays a - z, A - Z)?
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num = 0;
+
+while (num < 10)
+{
+putchar((num % 10) + '0');
+
+if (num != 9) 
+{
+putchar(',');
+putchar(' ');
+}
+
+num++;
+}
+
+putchar('\n');
+
+return (0);
+}
