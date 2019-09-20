d92a469b474ecbe5fb54540e39a209eed8d9d5b2
waffle <asuka9767@protonmail.com>
2019-09-20 00:09:28 +0000

    fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ else if (n % 10 == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, n % 10);
 }
-else if (n % 10 <= 5)
+else if (n % 10 <= 6)
 {
 printf("Last digit of %d is %d and less than 6 and not 0\n", n, n % 10);
 }
