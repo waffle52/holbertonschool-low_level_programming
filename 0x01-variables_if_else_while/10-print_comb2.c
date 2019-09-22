e9f49c6ccd4695cce170ce28f148ca3586b8b54f
waffle <asuka9767@protonmail.com>
2019-09-22 04:16:31 +0000

    updating to betty style

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,16 +9,18 @@
 int main(void)
 {
 
-int i; 
-for(i = 0; i <= 99; i++){
+int i;
+for (i = 0; i <= 99; i++)
+{
 putchar((i / 10) + '0');
 putchar((i % 10) + '0');
-if(i < 99)
+if (i < 99)
 {
 putchar(',');
 putchar(' ');
 }
 }
+putchar('\n');
 
 return (0);
 }
