e144295dace79754fd75b03e65000f68291ca979
waffle <asuka9767@protonmail.com>
2019-09-22 04:13:55 +0000

    uploading file 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: loops through and displays numbers 0 to 99)?
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int i; 
+for(i = 0; i <= 99; i++){
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+if(i < 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+
+return (0);
+}
