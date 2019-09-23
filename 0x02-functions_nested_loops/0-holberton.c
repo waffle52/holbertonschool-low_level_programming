f879457889f9e3925d524e1777db85b9f1b29162
waffle <asuka9767@protonmail.com>
2019-09-23 21:45:42 +0000

    uploading file 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * Description: prints Holberton)?
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+char ch[] = "Holberton";
+int i;
+
+for (i = 0; i < 9; i++)
+_putchar(ch[i]);
+
+
+_putchar('\n');
+
+return (0);
+}
