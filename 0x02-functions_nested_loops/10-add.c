04fa38043e047971ff25a9cd58f7b044609a119b
waffle <asuka9767@protonmail.com>
2019-09-25 15:48:10 +0000

    adding file 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - Entry point
+ * @a: int value
+ * @b: int value
+ * Description: add two numbers)?
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+
+return (a + b);
+}
