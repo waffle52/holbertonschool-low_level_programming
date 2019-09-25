e32bd903a830cadcf7e193727430bc6d4d64db52
waffle <asuka9767@protonmail.com>
2019-09-25 03:40:10 +0000

    uploading file 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - Entry point
+ * Description: gets every number from 00:00 to 23:59)?
+ * Return: numbers 00:00 to 23:59
+ */
+
+void jack_bauer(void){
+	int num = 0;
+	int num2;
+
+	for(num = 0; num < 24; num++){ 
+	       
+
+
+		for(num2 = 0; num2 < 60; num2++){
+
+		       
+			if (num < 10){
+			   _putchar('0');
+			   _putchar((num % 10) + '0');
+			} else {
+				_putchar((num / 10) + '0');
+				_putchar((num % 10) + '0');
+			}
+			
+			_putchar(':');
+			
+			if(num2 < 10){
+				_putchar('0');
+				_putchar((num2 % 10) + '0');
+			} else {
+				_putchar((num2 / 10) + '0');
+				_putchar((num2 % 10) + '0');
+			}
+
+			_putchar('\n');
+		}
+		
+
+	} 
+
+}
