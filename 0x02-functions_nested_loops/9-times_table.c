1ed7a3484e8d2713d5d752268bb2955c58161b4b
waffle <asuka9767@protonmail.com>
2019-09-25 20:01:12 +0000

    fixed file 9 to betty style

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,30 +13,37 @@ void times_table(void)
 	int x;
 	int y;
 
-	for(y = 0; y <= 9; y++){
-		for(x = 0; x <= 9; x++){
-			num = x * y;
+for (y = 0; y <= 9; y++)
+{
+for (x = 0; x <= 9; x++)
+{
+num = x * y;
 
-			if(num < 10 && x != 0){
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(num + '0');
-			}
-			else if (num >= 10){
-				_putchar(' ');
-				_putchar(num / 10 + '0');
-				_putchar(num % 10 + '0');
-			} else {
-				_putchar(num + '0');
-			}
+if (num < 10 && x != 0)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(num + '0');
+}
+else if (num >= 10)
+{
+_putchar(' ');
+_putchar(num / 10 + '0');
+_putchar(num % 10 + '0');
+}
+else
+{
+_putchar(num + '0');
+}
 
-			if( x != 9){
-				_putchar(',');
-			}
+if (x != 9)
+{
+_putchar(',');
+}
 
-		}
-		_putchar('\n');
-	}
+}
+_putchar('\n');
+}
 
 
 }
