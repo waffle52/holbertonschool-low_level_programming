93d198c91ab256ec542c27cff105ead1904defa0
waffle <asuka9767@protonmail.com>
2019-09-25 19:55:48 +0000

    uploading file 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,40 +9,34 @@
 
 void times_table(void)
 {
-	int num1;
-	int num2;
-	int num3;
+	int num;
+	int x;
+	int y;
 
-for (num1 = 0; num1 < 10; num1++)
-{
-for (num2 = 0; num2 < 10; num2++)
-{
-num3 = num1 * num2;
-if (num3 < 10)
-{
-_putchar((num3 % 10) + '0');
-if (num2 != 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-}
-}
+	for(y = 0; y <= 9; y++){
+		for(x = 0; x <= 9; x++){
+			num = x * y;
 
-else
-{
-_putchar((num3 / 10) + '0');
-_putchar((num3 % 10) + '0');
-if (num2 != 9)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
+			if(num < 10 && x != 0){
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(num + '0');
+			}
+			else if (num >= 10){
+				_putchar(' ');
+				_putchar(num / 10 + '0');
+				_putchar(num % 10 + '0');
+			} else {
+				_putchar(num + '0');
+			}
 
+			if( x != 9){
+				_putchar(',');
+			}
 
-}
-_putchar('\n');
-}
+		}
+		_putchar('\n');
+	}
+
 
 }
