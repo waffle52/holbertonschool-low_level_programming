bf70738a4619c3353c7c50038b2b76ae86071c7f
waffle <asuka9767@protonmail.com>
2019-09-26 17:15:36 +0000

    uploading file 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - Entry point
+ * @c: value of int
+ * Description: return 0 or 1?
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 58)
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
