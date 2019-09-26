11a5e4460d2f5007eff95f7c37c6edbc84cb8851
waffle <asuka9767@protonmail.com>
2019-09-26 21:38:34 +0000

    uploading file 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: replace 0 to 100 with words or print number)?
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0) /* 3 and 5 */
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0) /* 3 */
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0) /* 5 */
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%i ", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
