c82d355183c892c6052d7148ad9b33bd791db3f8
waffle <asuka9767@protonmail.com>
2019-09-26 17:19:49 +0000

    uploading file 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - Entry point
+ * @a: value of int
+ * @b: value of int
+ * Description: return a * b)?
+ * Return: a * b (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
