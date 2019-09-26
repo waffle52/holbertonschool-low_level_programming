f5b5225ef938fb755dcd1caa9624b4642d638cfd
waffle <asuka9767@protonmail.com>
2019-09-26 19:06:30 +0000

    uploading file 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Entry point
+ * Description: prints 0 to 9)?
+ */
+
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+	_putchar(i + '0');
+}
+
+_putchar('\n');
+}
