9977ba266a0cd36d05dc30836242a6b8e9669758
waffle <asuka9767@protonmail.com>
2019-09-30 22:07:37 +0000

    uploading file 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/*
+ * reset_to_98 - Entry point
+ * @n: Value of Pointer
+ * Description: assigns new value with pointer)?
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
