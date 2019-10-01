0887e4d2eff8c9f13cf6e16de4af626b4c63431c
waffle <asuka9767@protonmail.com>
2019-10-01 16:23:57 +0000

    uploading file 1 and header

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * swap_int - Entry point
+ * @a: pointer to int a
+ * @b: pointer to int b
+ * Description: swaps poitner values)?
+ */
+
+void swap_int(int *a, int *b)
+{
+
+	int c = *a;
+	*a = *b;
+	*b = c;
+
+
+}
