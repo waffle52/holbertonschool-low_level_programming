421174a2906383e8906fda0119ac22fef2704713
waffle <asuka9767@protonmail.com>
2019-10-02 06:05:36 +0000

    changed output

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,9 +10,12 @@ void puts_half(char *str)
 {
 	int i;
 
-	for (i = _strlen(str) / 2; i <= _strlen(str) - 1; i++)
+	for (i = (_strlen(str) + 1) / 2; str[i] != '\0'; i++)
 	{
+		if ((_strlen(str) + 1) / 2)
+		{
 		_putchar(str[i]);
+		}
 	}
 
 	_putchar('\n');
