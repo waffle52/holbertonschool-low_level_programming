5f86f666a37943874565538619099f7a2c85522a
waffle <asuka9767@protonmail.com>
2019-10-02 06:37:21 +0000

    uploading file 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - Entry point
+ * @dest: value of character
+ * @src: value of character
+ * Description: copies string)?
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int k;
+
+	for (k = 0; k < _strlen(src); k++)
+	{
+		dest[k] = src[k];
+	}
+
+	return (dest);
+}
+
+/**
+ * _strlen - Entry point
+ * @s: value of char
+ * Description: return string length)?
+ * Return: string length
+ */
+
+		    int _strlen(char *s)
+		{
+			int i = 0;
+
+			while (*s != '\0')
+			{
+				s++;
+				i++;
+			}
+
+			return (i);
+		}
