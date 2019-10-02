fa84a778ebd1cbae4013dbb9e1495dcfc4cd3d54
waffle <asuka9767@protonmail.com>
2019-10-02 03:19:02 +0000

    uploading file 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * puts_half - Entry point
+ * @str: value of character
+ * Description: prints half of a string)?
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = _strlen(str) / 2; i <= _strlen(str) - 1; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
+
+/**
+ * _strlen - Entry point
+ * @s: value of char
+ * Description: return string length)?
+ * Return: string length
+ */
+
+	int _strlen(char *s)
+	{
+		int i = 0;
+
+		while (*s != '\0')
+		{
+			s++;
+			i++;
+		}
+
+		return (i);
+	}
