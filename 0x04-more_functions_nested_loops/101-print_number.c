ed3fcb16ea08dfe8b07c1d273637275ac0903aee
waffle <asuka9767@protonmail.com>
2019-10-02 01:38:28 +0000

    uploading file 6

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * print_number - Entry point
+ * @n: value of integer
+ * Description: prints an int)?
+ */
+
+void print_number(int n)
+{
+	_putchar((n % 10) + '0');
+}
