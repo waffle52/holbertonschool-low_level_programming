80e2464c6a084730a19df10232e3a8d3c72ecb3f
waffle <asuka9767@protonmail.com>
2019-10-03 21:42:27 +0000

    stopped loop and seg fault

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,10 +18,11 @@ char *_strncat(char *dest, char *src, int n)
 
 	}
 
-	for (j = 0; j < n; ++j, ++i)
+	for (j = 0; j < n && src[j] != '\0'; ++j, ++i)
 	{
 		dest[i] = src[j];
 	}
+	dest[i] = '\0';
 
 	return (dest);
 }
