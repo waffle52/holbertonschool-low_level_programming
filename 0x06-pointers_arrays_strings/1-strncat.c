6f207fccb2da71e79f7670472f110c38f33a9a0d
waffle <asuka9767@protonmail.com>
2019-10-03 21:20:46 +0000

    uploading file 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * *_strncat - Entry point
+ * @dest: value of char
+ * @src: value of char
+ * @n: value of int
+ * Description: adds two strings)?
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; ++i)
+	{
+
+	}
+
+	for (j = 0; j < n; ++j, ++i)
+	{
+		dest[i] = src[j];
+	}
+
+	return (dest);
+}
