8cbfd8b0b35a10a4c1a9eb6d580133027b8a9d33
waffle <asuka9767@protonmail.com>
2019-10-07 16:47:10 +0000

    uploading file 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * *_memset - Entry point
+ * @s: pointer of char
+ * @b: value of char
+ * @n: unsigned int
+ * Description: fills memory and shares location)?
+ * Return: pointer to memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+
+	}
+
+	return (s);
+}
