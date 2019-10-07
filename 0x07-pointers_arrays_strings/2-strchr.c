e1fd8ae57f66f57dfeaed0ed2cc3feb523ba8dff
waffle <asuka9767@protonmail.com>
2019-10-07 20:34:30 +0000

    uploading file 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * *_strchr - Entry point
+ * @s: pointer of char
+ * @c: char
+ * Description: locates a char in a string)?
+ * Return: pointer of char
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return ('\0');
+}
