9a9dfb53d3479717d0dac3a760c271236b55e3de
waffle <asuka9767@protonmail.com>
2019-10-09 06:50:55 +0000

    uploading file 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _strspn - Entry point
+ * @s: pointer of char
+ * @accept: pointer of char
+ * Description: returns number of bytes in char s)?
+ * Return: unsigned integer
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int counter;
+	int i;
+	int j;
+
+	for (i = 0; s[i] != ','; i++)
+	{
+
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				counter++;
+			}
+
+
+		}
+	}
+
+
+	return (counter);
+}
