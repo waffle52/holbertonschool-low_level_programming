3810877d9776af6d8253fc6e7b88809eae77cef2
waffle <asuka9767@protonmail.com>
2019-10-09 06:52:58 +0000

    changed loop to end at byte

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,7 +17,7 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i] != ','; i++)
 	{
 
-		for (j = 0; accept[j]; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
