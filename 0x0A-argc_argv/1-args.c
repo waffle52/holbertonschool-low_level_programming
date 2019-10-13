d48ab77dbc0e9137db4f4e60a4f444a0047e9286
waffle <asuka9767@protonmail.com>
2019-10-13 01:51:58 +0000

    uploading file 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: length of argv[]
+ * @argv: pointer to char of arguments
+ * Description: prints number of arguments passed)?
+ * Return: 0 (SUCCESS)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+
+	printf("%i", argc - 1);
+	printf("\n");
+
+	return (0);
+}
