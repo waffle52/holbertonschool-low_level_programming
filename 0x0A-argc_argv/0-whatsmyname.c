70d3cb911d9391094fa78a249107fb7180fc769b
waffle <asuka9767@protonmail.com>
2019-10-13 01:42:42 +0000

    uploading file 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: length of argv
+ * @argv: pointer to char of arguments
+ * Description: prints program name from argv)?
+ * Return: 0 (SUCCESS)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s", argv[0]);
+	printf("\n");
+
+	return (0);
+}
