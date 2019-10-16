592252fd549490c31c39355ed6ee486e1ac278c8
waffle <asuka9767@protonmail.com>
2019-10-16 19:52:35 +0000

    added comments

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -2,25 +2,27 @@
 
 /**
  * main - Entry point
- * Description: loops through and displays numbers 0 to 99)?
- * Return: Always 0 (Success)
+ * Description: prints 00 to 99)?
+ * Return: 0 (SUCCESS)
  */
 
 int main(void)
 {
+	int i;
 
-int i;
-for (i = 0; i <= 99; i++)
-{
-putchar((i / 10) + '0');
-putchar((i % 10) + '0');
-if (i < 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
+	for (i = 0; i <= 99; i++)
+	{
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
 
-return (0);
+		if (i < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
