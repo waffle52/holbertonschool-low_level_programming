d1837d80f1660fb39310f24ca27f119ec8a8cc88
waffle <asuka9767@protonmail.com>
2019-10-17 22:46:21 +0000

    changed for loop

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,23 +36,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	size = _strlen(s1) + n;
 	}
 
-	str = malloc(sizeof(char) * size + 1);
+	str = malloc((sizeof(char) * (size + 1)));
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; i < _strlen(s1); i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		str[i] = s1[i];
 	}
 
-	for (j = 0; i < size; j++, i++)
+	for (j = 0; j < n; j++)
 	{
-		str[i] = s2[j];
+		str[i + j] = s2[j];
 	}
-	str[i + 1] = '\0';
+
+	str[i + j] = '\0';
 
 	return (str);
 
