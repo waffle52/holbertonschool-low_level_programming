425e8a23e7ef41166dbb39cac39342f3e4e2ea9d
waffle <asuka9767@protonmail.com>
2019-10-17 06:46:22 +0000

    uploading file 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - Entry point
+ * @grid: pointer to pointer of integer
+ * @height: columns of array
+ * Description: free up 2D array in memory)?
+ * Return: none
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (height <= 0 || grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+
+	}
+	free(grid);
+}
