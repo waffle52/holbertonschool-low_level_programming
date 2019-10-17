c8182ed9828b34a568df0fde0acc683772c095d0
waffle <asuka9767@protonmail.com>
2019-10-17 22:25:41 +0000

    uploading file 1 and updated header

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -2,6 +2,7 @@
 #define _HOLBERTON_H_
 
 int _putchar(char c);
+unsigned int _strlen(char *s);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
