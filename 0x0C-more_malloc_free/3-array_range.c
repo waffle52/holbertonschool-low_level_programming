db00a74e9eae1528997216d0afffd66f4f7a58dc
waffle <asuka9767@protonmail.com>
2019-10-18 19:21:17 +0000

    uploading file 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *array_range - Entry point
+ * @min: integer of min value
+ * @max: integer of max value
+ * Description: creates an array of integers)?
+ * Return: pointer to int array
+ */
+
+int *array_range(int min, int max)
+{
+	int *str;
+	int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	str = malloc(sizeof(int) * (max + 1));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; min < max + 1; i++, min++)
+	{
+		str[i] = min;
+	}
+
+	return (str);
+}
