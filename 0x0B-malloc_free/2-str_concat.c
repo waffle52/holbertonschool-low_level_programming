f3c3802c818fbb2de8dbf8f1b087dfe075254674
waffle <asuka9767@protonmail.com>
2019-10-20 08:40:27 +0000

    fixed to betty style and added empty strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,36 +18,50 @@ char *str_concat(char *s1, char *s2)
 
 	size = 0;
 
-	size = _strlen(s1) + _strlen(s2) + 1;
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	size = _strlen(s1) + _strlen(s2);
 
 
 	str = malloc(sizeof(char) * size);
 
-	if(str == '\0')
+	if (str == NULL)
 	{
 		return ('\0');
 	}
 
-	for(i = 0; i < _strlen(s1); i++)
+	for (i = 0; i < _strlen(s1); i++)
 	{
 		str[i] = s1[i];
 	}
 
-	for(j = 0; j < _strlen(s2); i++, j++)
+	for (j = 0; j < _strlen(s2); i++, j++)
 	{
 		str[i] = s2[j];
 	}
-	str[i] = '\0';
-
-
+	str[i + 1] = '\0';
 
 	return (str);
 }
 
+/**
+ * _strlen - length of string
+ * @s: pointer of char
+ * Return: integer of length
+ */
 
 int _strlen(char *s)
 {
 	int i = 0;
+
 	while (*s != '\0')
 	{
 		s++;
