4a84225ce1122ee97bce1a10fcb7713006775ff6
waffle <asuka9767@protonmail.com>
2019-10-20 09:06:13 +0000

    fixed to betty style and redid loops

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,35 +15,39 @@ int **alloc_grid(int width, int height)
 	int x;
 	int y;
 
-	if(height <= 0 || height <= 0)
+	if (height <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 
 	grid = malloc(sizeof(int) * height);
 
-	if(grid == NULL)
+	if (grid == NULL)
 	{
 		return (NULL);
 	}
 
-	for(x = 0; grid[x]; x++)
+	for (x = 0; x < height; x++)
 	{
 		grid[x] = malloc(sizeof(int) * width);
 
-		if(grid[x] == NULL)
+		if (grid[x] == NULL)
 		{
-			free(grid[x]);
+			for (k = 0; k < height; k++)
+			{
+				free(grid[k]);
+			}
 			free(grid);
 			return (NULL);
 		}
-
-	for(y = 0; grid[x][y]; y++)
-	{
-		grid[x][y] = 0;
 	}
 
+	for (x = 0; grid[x]; x++)
+	{
+		for (y = 0; grid[x][y]; y++)
+		{
+			grid[x][y] = 0;
+		}
 	}
 	return (grid);
-
 }
