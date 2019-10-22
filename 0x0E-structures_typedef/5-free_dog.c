2a2e0134b8082b9fe243f6e9a1f911f0e6f657c1
waffle <asuka9767@protonmail.com>
2019-10-22 20:25:39 +0000

    uploading file 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Entry point
+ * @d: pointer of dog_t
+ * Description: free memory by d)?
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	free(dog->name);
+	free(dog->owner);
+	free(d);
+
+}
