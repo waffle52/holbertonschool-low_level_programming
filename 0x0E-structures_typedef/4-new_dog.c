bf099b5cf4c4c876d39e43f697c7e25eea87b424
waffle <asuka9767@protonmail.com>
2019-10-22 17:50:35 +0000

    fixed if statement

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,30 +2,83 @@
 #include <stdlib.h>
 
 /**
+ * _strcpy - Copies a string from src to dest
+ * @dest: String to be copied to
+ * @src: String to be copied from
+ *
+ * Return: Pointer to start of dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
+
+/**
+ * _strlen - Gets the length of the input string
+ * @str: String input
+ * Return: Number of indexes in a string
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		;
+
+	return (i);
+}
+
+/**
  * *new_dog - Entry point
  * @name: pointer of char
  * @age: float
  * @owner: pointer of char
  * Description: copies data)?
- * Return: memory dog of type dog_t
+ * Return: memory of type dog_t
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	dog_t *newDog = NULL;
+	char *newName;
+	char *newOwner;
+	int nameLen;
+	int ownerLen;
 
-	dog = malloc(sizeof(dog_t));
-
-	if (dog == NULL)
+	if (name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
+	newDog = malloc(sizeof(dog_t));
+	if (newDog == NULL)
+		return (NULL);
 
-	dog->name = name;
+	nameLen = _strlen(name);
+	newName = malloc(sizeof(char) * (nameLen + 1));
+	if (newName == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
 
-	dog->age = age;
+	ownerLen = _strlen(owner);
+	newOwner = malloc(sizeof(char) * (ownerLen + 1));
+	if (newOwner == NULL)
+	{
+		free(newName);
+		free(newDog);
+		return (NULL);
+	}
 
-	dog->owner = owner;
+	newDog->name = _strcpy(newName, name);
 
-	return (dog);
+	newDog->age = age;
+
+	newDog->owner = _strcpy(newOwner, owner);
+
+	return (newDog);
 }
