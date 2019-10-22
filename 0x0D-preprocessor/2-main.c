9369e625bbbda564e67ba44c0d7cb836b98fdb01
waffle <asuka9767@protonmail.com>
2019-10-22 01:27:50 +0000

    uploading file 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define file __FILE__
+
+/**
+ * main - Entry point
+ * Description: prints file name)?
+ * Return: 0 (EXIT_SUCCESS)
+ */
+
+int main(void)
+{
+	printf("%s\n", file);
+
+	return (0);
+}
