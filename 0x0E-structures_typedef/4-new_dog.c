aef2f7f3c75787e0a68163f75dc8aeba49c82936
waffle <asuka9767@protonmail.com>
2019-10-22 06:37:32 +0000

    fixed malloc to size of dog_t

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,15 +14,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
-	dog = malloc(sizeof(name) + sizeof(age) + sizeof(owner));
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 	{
 		return (NULL);
 	}
 
 	dog->name = name;
+
 	dog->age = age;
+
 	dog->owner = owner;
+
 
 return (dog);
 }
