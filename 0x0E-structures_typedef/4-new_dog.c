60944ff58330d04a0beeae8edefc587b7218fa16
waffle <asuka9767@protonmail.com>
2019-10-22 07:30:19 +0000

    added if statements

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,12 +20,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-
+	if (name != NULL)
+	{
 	dog->name = name;
+	}
 
 	dog->age = age;
 
+	if(owner != NULL)
+	{
 	dog->owner = owner;
+	}
+
 
 	return (dog);
 }
