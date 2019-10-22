6932e757b2e525177e7f3e6104c09f14643e4b57
waffle <asuka9767@protonmail.com>
2019-10-22 07:21:57 +0000

    removed last changes

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,19 +14,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
-
 	dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
 	{
 		return (NULL);
 	}
-	dog{
+
 	dog->name = name;
 
 	dog->age = age;
 
 	dog->owner = owner;
-	}
+
 	return (dog);
 }
