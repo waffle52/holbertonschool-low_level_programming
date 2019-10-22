4479065211db3a6d2572087c07cacd85ab647e28
waffle <asuka9767@protonmail.com>
2019-10-22 18:20:58 +0000

    added prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,6 +21,8 @@ typedef struct dog dog_t;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+int _strlen(char *str);
+char *_strcpy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
