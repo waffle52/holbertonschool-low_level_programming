9cadcdc10486200e667c6d1bc0194037ca3a8921
waffle <asuka9767@protonmail.com>
2019-10-22 04:29:13 +0000

    Updated header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,13 +14,14 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+} dogt_t;
+
+typedef struct dog dog_t;
 
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*
- *dog_t *new_dog(char *name, float age, char *owner);
- *void free_dog(dog_t *d);
-*/
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
