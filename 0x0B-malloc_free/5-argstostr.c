71c4ac86f3254bf4754f73d32b7f687c3b7be622
waffle <asuka9767@protonmail.com>
2019-10-26 01:53:25 +0000

    uploading advanced file

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,102 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *string_nconcat - Entry point
+ * @s1: pointer of char
+ * @s2: pointer of char
+ * @n: number for malloc size
+ * Description: concatenates two strings)?
+ * Return: pointer to string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int size, i, j, length1, length2;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	length1 = _strlen(s1);
+	length2 = _strlen(s2);
+
+	if (n >= length2)
+	{
+		n = length2;
+	}
+
+	size = length1 + n;
+	str = malloc((sizeof(char) * (size + 1)));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		str[i] = s1[i];
+	}
+	for (j = 0; j < n; j++)
+	{
+		str[i + j] = s2[j];
+	}
+	str[i + j] = '\0';
+
+	return (str);
+}
+
+/**
+ * _strlen - Entry point
+ * @s: pointer to char
+ * Description: gets length of passed in string)?
+ * Return: unsigned integer of length s
+ */
+
+unsigned int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+
+	return (i);
+}
+/**
+ * memClear - Frees memory
+ * @s: Stuff
+ * Return: Nothing
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i;
+	char *str;
+
+	str = malloc(sizeof(char *) * (_strlen() + 1));
+
+	if (str == NULL || ac == 0)
+	{
+		return (NULL);
+	}
+
+	for(i = 0; i < ac; i++)
+	{
+		/* call concat" 
+	}
+	str[i] = '\0';
+
+	return (str);
+}
