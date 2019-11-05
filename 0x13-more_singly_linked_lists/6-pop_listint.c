6c2a72cff0799d05ad64d0f3df00b61a26dbe5e8
waffle <asuka9767@protonmail.com>
2019-11-05 23:53:51 +0000

    uploading file 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Entry Point
+ * @head: address of head node
+ * Description: deletes the head node and returns the data of the head node)?
+ * Return: head nodes data
+ */
+
+int pop_listint(listint_t **head)
+{
+	int num = 0;
+	listint_t *temp = *head;
+	listint_t *holder;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	if (temp != NULL)
+	{
+		holder = temp->next;
+		free(temp);
+		*head = holder;
+	}
+
+	return (num);
+}
