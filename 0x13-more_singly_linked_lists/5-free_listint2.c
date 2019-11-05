3a0faf9a8c806ed3d69cd98b9797e1d8252dda71
waffle <asuka9767@protonmail.com>
2019-11-05 06:27:34 +0000

    uploading file 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - Entry Point
+ * @head: address of head node
+ * Description: loops through each nodes and free the allocated memory)?
+ */
+
+void free_listint2(listint_t **head)
+{
+	*head = NULL;
+}
