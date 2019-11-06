86a7b0af1c59ee7787af3aa1bd37d9a752ad0840
waffle <asuka9767@protonmail.com>
2019-11-06 03:52:41 +0000

    uploading file 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Entry Point
+ * @head: address of head node
+ * @idx: index to add node
+ * @n: number to assign to new node
+ * Description: creates a new node at the given index)?
+ * Return: the new node created
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node;
+	listint_t *tmp = *head;
+	unsigned int i = 0;
+
+	if (head == NULL || *head == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+
+	for (i = 0; tmp != NULL; i++)
+	{
+		if (idx == 0)
+		{
+			new_node->n = n;
+			new_node->next = *head;
+			*head = new_node;
+			return (new_node);
+		}
+		else if (i == (idx - 1))
+		{
+			new_node->n = n;
+			new_node->next = tmp->next;
+			tmp->next = new_node;
+			return (new_node);
+		}
+
+		tmp = tmp->next;
+	}
+
+	free(new_node);
+	return (NULL);
+}
