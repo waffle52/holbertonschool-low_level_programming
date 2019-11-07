d8276d86b2830618572729ea105af25a260cef85
waffle <asuka9767@protonmail.com>
2019-11-07 06:32:24 +0000

    uploading file 100

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * reverse_nodeint_at_index - Entry Point
+ * @head: address of head node
+ * Description: reverses a list and returns head of new list)?
+ * Return: pointer to first node of reversed list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
