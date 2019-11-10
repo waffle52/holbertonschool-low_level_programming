e3e7c37f56a87af9799edf7d7ab9451744fef555
waffle52 <asuka9767@protonmail.com>
2019-11-10 11:54:06 +0000

    removed main parameters

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,11 @@
 
 /**
  * main - Entry point
- * @argc: length of argv[]
- * @argv: pointer to char of arguments
  * Description: prints string to the standard error)?
  * Return: 1 (SUCCESS)
  */
 
-int main(int argc, char *argv[])
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
