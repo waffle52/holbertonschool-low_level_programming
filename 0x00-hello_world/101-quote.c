19b5d6c1265db2d4b8c3dae4fb2737bac5d201c3
waffle52 <asuka9767@protonmail.com>
2019-11-10 11:36:39 +0000

    added fwrite function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,15 @@
  * main - Entry point
  * @argc: length of argv[]
  * @argv: pointer to char of arguments
- * Description: prints all arguments given)?
- * Return: 0 (SUCCESS)
+ * Description: prints string to the standard error)?
+ * Return: 1 (SUCCESS)
  */
 
-int main(void){
-2
+int main(int argc, char *argv[])
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(str, 1, sizeof(str), stderr);
+
+	return (1);
 }
