282f5b179147cc870e94a29aee2d7f35650d2ced
waffle52 <asuka9767@protonmail.com>
2019-11-10 10:01:30 +0000

    push test

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * @argc: length of argv[]
+ * @argv: pointer to char of arguments
+ * Description: prints all arguments given)?
+ * Return: 0 (SUCCESS)
+ */
+
 int main(void){
 
 }
