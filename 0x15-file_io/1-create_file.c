030fe7b02aa7cdcf950f5b94775238a1ca22ccf9
waffle52 <asuka9767@protonmail.com>
2019-11-13 10:24:01 +0000

    made length be 0 at default

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, read_num, written, cl;
-	int length = strlen(text_content);
+	int length = 0 + strlen(text_content);
 	char *buf;
 
 	if (filename == NULL)
