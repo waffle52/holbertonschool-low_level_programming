a6b3ab553792a1a770a9a533923dfc9dde6fdc2b
waffle52 <asuka9767@protonmail.com>
2019-11-13 10:44:32 +0000

    added truncate flag

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		free(buf);
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		free(buf);
