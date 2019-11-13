a1334db1f92353457bc4444cc80064eb4c364d0b
waffle52 <asuka9767@protonmail.com>
2019-11-13 11:26:50 +0000

    uploading file 2 for testing

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,60 @@
+#include "holberton.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * append_text_to_file - Entry Point
+ * @filename: name of file to be created
+ * @text_content: String to write to file
+ * Description: appends text to a file)?
+ * Return: 1 (SUCCESS)
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, read_num, written, cl;
+	int length = 0;
+	char *buf;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+		length = strlen(text_content);
+	buf = malloc(sizeof(char) * length);
+	if (buf == NULL)
+	{
+		free(buf);
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_APPEND, 0600);
+	if (fd == -1)
+	{
+		free(buf);
+		return (-1);
+	}
+	written = write(fd, text_content, length);
+	if (written == -1)
+		return (-1);
+	cl = close(fd);
+	if (cl == -1)
+		return (-1);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (-1);
+	}
+	read_num = read(fd, buf, length);
+	if (read_num == -1)
+		return (-1);
+	cl = close(fd);
+	if (cl == -1)
+		return (-1);
+	free(buf);
+	return (1);
+}
