fc8591da1749618490634363e6c89809798d16eb
waffle52 <asuka9767@protonmail.com>
2019-11-13 10:16:27 +0000

    uploading file for testing, no spaces due to betty style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		free(buf);
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
 	if (fd == -1)
 	{
 		free(buf);
@@ -50,7 +50,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	read_num = read(fd, buf, length);
-	if (read_num)
+	if (read_num == -1)
 		return (-1);
 	cl = close(fd);
 	if (cl == -1)
