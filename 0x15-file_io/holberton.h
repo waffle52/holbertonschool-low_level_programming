531a7d44f6839df2d401ef02de0815a073086110
waffle52 <asuka9767@protonmail.com>
2019-11-13 06:10:52 +0000

    added library

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,5 +1,6 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
