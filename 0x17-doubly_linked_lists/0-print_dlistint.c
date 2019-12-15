a608089a3e94d113830bb7558cd13f719edd4069
waffle <asuka9767@protonmail.com>
2019-12-15 08:46:51 +0000

    Added test case for head is NULL

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint(const dlistint_t *h)
+ * print_dlistint - Entry Point
  * @h: head node of the linked list
  * Description: Prints all elements of linked list given)?
  * Return: number of elements in linked list
@@ -11,11 +11,16 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t counter = 0;
 
+	if (h == NULL)
+	{
+		return (0);
+	}
+
 	while (h != NULL)
 	{
 		printf("%i\n", h->n);
-		counter++;
 		h = h->next;
+		counter++;
 	}
 
 	return (counter);
