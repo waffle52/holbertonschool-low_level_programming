059ea3b325baa7c0485d5d2a18c480a9b7c0fafc
waffle <asuka9767@protonmail.com>
2020-01-18 20:17:07 +0000

    uploading for testing

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_create - Entry Point
+ * @size: size of the array
+ * Description: Creates a hash table
+ * Return: a pointer to hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		printf("Table is NULL");
+		return (NULL);
+	}
+
+	table->array = malloc(size * sizeof(hash_node_t));
+	if (table->array == NULL)
+	{
+		printf("Table array is NULL\n");
+		return (NULL);
+	}
+
+	/*memset(table->array, 0, size * sizeof(hash_node_t));*/
+
+	table->size = size;
+
+	return (table);
+}
