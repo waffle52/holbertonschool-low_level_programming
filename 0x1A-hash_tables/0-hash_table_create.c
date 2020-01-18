da03b0232a3c44bb5a5132a170ba29190a2d2b9e
waffle <asuka9767@protonmail.com>
2020-01-18 21:51:59 +0000

    removed comment

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -28,8 +28,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	/*memset(table->array, 0, size * sizeof(hash_node_t));*/
-
 	table->size = size;
 
 	return (table);
